24f451bce3537daa457bdebe75ddfe1f171964cc
Dorothyhung <dorothyh0602@gmail.com>
2023-08-09 23:45:44 -0700

    functions

--- WebScraper.py
+++ WebScraper.py
@@ -1,57 +1,49 @@
 from bs4 import BeautifulSoup
 import requests
-import csv
 from collections import Counter
-
-
 import nltk
 #nltk.download('all')
 from nltk import FreqDist
-#from ntlk.corpus import stopwords
 from textblob import TextBlob
 
+def collect_posts(url):
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    print('Creating requests ...')
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    posts = soup.find_all("div", class_='thing')
+    return posts, headers
 
+def collect_text(post, headers):
+    title = post.find('p', class_="title").text.replace('(self.confessions)', '')
+    print(title)
+    post_path = post.find("a", class_='title may-blank')['href']
+    post_url = f'https://old.reddit.com{post_path}'
+    post_content = requests.get(post_url, headers=headers)
+    soup = BeautifulSoup(post_content.text, 'html.parser')
+    site_table = soup.find("div", class_='sitetable linklisting')
+    post_content = site_table.find('div', class_='thing').find('div', class_='entry unvoted').find_all('p')
+    print_sentiment(post_content)
 
-url = 'https://old.reddit.com/r/confessions/top/?sort=top&t=all'
-headers = {'User-Agent': 'Mozilla/5.0'}
-print('Creating requests ...')
-page = requests.get(url, headers=headers)
-soup = BeautifulSoup(page.text, 'html.parser')
-posts = soup.find_all("div", class_='thing')
-words = ""
-print("Gathering post content ...")
-for post in posts:
+def print_upvotes(post):
     title = post.find('p', class_="title").text.replace('(self.confessions)', '')
     print(title)
     upvotes = post.find("div", attrs={"class": "score likes"}).text
     if upvotes == "•":
         upvotes = "None"
     result = f'liked {upvotes} times. Title: {title}'
+    print(result)
 
-
-    post_link = post.find("a", class_='title may-blank')['href']
-    #print(post_link)
-    post_url = f'https://old.reddit.com{post_link}'
-    post_content = requests.get(post_url, headers=headers)
-    soup = BeautifulSoup(post_content.text, 'html.parser')
-    site_table = soup.find("div", class_='sitetable linklisting')
-    post_content = site_table.find('div', class_='thing').find('div', class_='entry unvoted').find_all('p')
+def print_sentiment(post_content):
     blob = TextBlob(str(post_content))
     sentiment = blob.sentiment.polarity
     print("Sentiment= ", sentiment)
-    for paragraph in post_content:
-        #print(paragraph.text)
-        words = words + paragraph.text
 
-print("Calculating word counts ...")
-#freq = Counter(words.split()).most_common()
-# freq = FreqDist(words.lower().split())
-# print(freq)
-# print(freq.most_common())
+def main():
+    url = 'https://old.reddit.com/r/confessions/top/?sort=top&t=all'
+    posts, headers = collect_posts(url)
+    for post in posts:
+        collect_text(post, headers)
 
-blob = TextBlob(words)
-sentiment = blob.sentiment.polarity
-print("Sentiment= ", sentiment)
-    # with open('./source/repos/WebScraper/redditconfessions.csv', 'a') as f:
-    #     writer = csv.writer(f)
-    #     writer.writerow([result])
+main()
+
